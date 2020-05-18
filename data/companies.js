ecfff987c0262650db4a63ebafcd38adf59d4bcf
Gaston Arbeletche <garbeletche@vairix.com>
2020-05-18 15:00:21 -0300

    add vairix company

--- data/companies.js
+++ data/companies.js
@@ -445,6 +445,15 @@ export default [
     meta: {
       allowFullRemote: true,
       hasPhysicalOffices: true
+    },
+    {
+    picture: "https://www.cuti.org.uy/data/f/c/socios/237/160x160/logo-png-transparente-160x160.png",
+    url: "https://www.vairix.com/",
+    name: "VAIRIX Software Development",
+    description: "Nearshore software development company that provides staff augmentation and custom software development services.",
+    meta: {
+      allowFullRemote: true,
+      hasPhysicalOffices: true
     }
   },
 ];
